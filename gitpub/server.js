4ac9371d869c60594d4c52d3076b24fdebe89c09
jcari-rgb <582119jc@gmail.com>
2021-04-16 12:25:29 -0400

    made changes to files so it displays welcome msg in localhost3000

---
+++ gitpub/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!')
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('01010111 01101111 01100001 01101000 00101100 00100000 01001000 01101001 00100000 01001110 01101111 01110010 01101001 00100001')
+});
